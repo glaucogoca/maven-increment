11075796cbd207ddd36a810a0f568dc3c8116bf8
Glauco Cardoso <glauco.cardoso@ericsson.com>
2023-02-09 16:33:42 -0300

    first commit

---
+++ src/test/java/com/ericsson/bss/mvnversionincrement/MvnVersionIncrementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ericsson.bss.mvnversionincrement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvnVersionIncrementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
